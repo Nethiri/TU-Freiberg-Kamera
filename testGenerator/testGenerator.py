2ee35ec6ae185d56a75023923bd204a18cc16f59
Nethiri <marconaumann@t-online.de>
2021-06-17 11:12:51 +0200

    reee

--- testGenerator/testGenerator.py
+++ testGenerator/testGenerator.py
@@ -1,2 +1,5 @@
+#this is going to be a program, that takes an image and lets you mark in an 2d array where there is path...
+#in the end you will be able to generate a datasample for a tensorflow project
+
 print("Hello World")
 print("Hello World 2 Test")
